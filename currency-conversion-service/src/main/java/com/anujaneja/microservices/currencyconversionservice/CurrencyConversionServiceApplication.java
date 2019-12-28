de9cac57377e95275adfb9116dbe42634b3871ef
Anuj Aneja <anuj.aneja@unicommerce.com>
2019-12-28 18:53:47 +0530

    Naming server is implemented and services are registered with naming server.

--- currency-conversion-service/src/main/java/com/anujaneja/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/anujaneja/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.anujaneja.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
 @EnableFeignClients("com.anujaneja.microservices.currencyconversionservice")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
