bab4ed481d2bb08e9627be28df4117b09947357d
Anuj Aneja <anuj.aneja@unicommerce.com>
2019-12-30 17:00:35 +0530

    Very basic hystrix fault tolerance example implemented.

--- limits-service/src/main/java/com/anujaneja/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/anujaneja/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,6 +1,7 @@
 package com.anujaneja.microservices.limitsservice;
 
 import com.anujaneja.microservices.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +15,15 @@ public class LimitsConfigurationController {
     @GetMapping("/limits")
     public LimitConfiguration retrieveConfigurations() {
         return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+    }
+
+    @GetMapping("/limits-fault-example")
+    @HystrixCommand(fallbackMethod = "returnFallbackDefaultConfig")
+    public LimitConfiguration retrieveConfigurationsWithFallback() {
+        throw new RuntimeException("");
+    }
+
+    public LimitConfiguration returnFallbackDefaultConfig() {
+        return new LimitConfiguration(999999,9);
     }
 }
--- limits-service/src/main/java/com/anujaneja/microservices/limitsservice/LimitsServiceApplication.java
+++ limits-service/src/main/java/com/anujaneja/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,8 +2,13 @@ package com.anujaneja.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
